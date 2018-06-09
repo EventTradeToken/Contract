79af51b2c1a5e2fbefd810a8498a391c943ed86b
jajul <jajulnex@mail.ru>
2018-06-09 22:55:00 +0300

    small fixed - added count to products

--- test/EventTradeToken.js
+++ test/EventTradeToken.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import {assertEquals} from './helpers/asserts';
+import {expectThrow} from './helpers/expectThrow';
 
 const EventTradeToken = artifacts.require('EventTradeToken.sol');
 
@@ -18,17 +19,29 @@ contract('EventTradeTokenTest', function (accounts) {
 
     it('new products', async function () {
         const token = await EventTradeToken.new();
-        await token.addProduct(1, 'T-Shirt', 20);
-        await token.addProduct(2, 'Mug', 10);
+        await token.addProduct(1, 'T-Shirt', 20, 100);
+        await token.addProduct(2, 'Mug', 10, 100);
         assertEquals(await token.getProductsCount(), 2);
 
         const product_array = await  token.getProductByIndex(1);
         assertEquals(product_array[0], 2);
         assertEquals(product_array[2], 10);
 
-        await token.addProduct(2, 'Mug2', 10);
-        await token.addProduct(3, 'Pen', 5);
+        await token.addProduct(2, 'Mug2', 10, 100);
+        await token.addProduct(3, 'Pen', 5, 100);
 
         assertEquals(await token.getProductsCount(), 3);
+    });
+
+    it('buy products', async function () {
+        const token = await EventTradeToken.new();
+        await token.addProduct(1, 'T-Shirt', 20, 1);
+        await token.addProduct(2, 'Mug', 70, 2);
+
+        await token.newClient("Julia");
+
+        // await expectThrow(token.buyProduct("Julia", 2));
+        // await token.buyProduct("Julia", 1);
+        // await expectThrow(token.buyProduct("Julia", 1));
     });
 });
